28ed1b8c999d620279f8881e75e0053c2a32c3c8
DevJhojan <jhojan1806@gmail.com>
2024-02-03 18:56:55 -0500

    second route and new files how "me"

--- AuthView/src/app/app.routes.ts
+++ AuthView/src/app/app.routes.ts
@@ -6,5 +6,13 @@ export const routes: Routes = [
     path: "",
     pathMatch:"full",
     loadComponent: () => import("./login/login.component"),
+  },
+  {
+    path: "me",
+    loadComponent: () => import("./me/me.component"),
+  },
+  {
+    path: "**",
+    redirectTo: "",
   }
 ];
